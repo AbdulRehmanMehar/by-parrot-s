3561498ba5c9e88c633597a1dbab8381a49b991a
Abdul Rehman <mehars.6925@gmail.com>
2019-03-19 14:47:14 +0500

    reducers for items

---
+++ src/store/reducers/itemReducer.js
@@ -0,0 +1,14 @@
+const init = {
+    items: [
+        { id: '1', title: 'Cocktail', price: 700, description: 'Lorem ipsum dolor sit amet.' },
+        { id: '2', title: 'Cocktoo', price: 700, description: 'Lorem ipsum dolor sit amet.' },
+        { id: '3', title: 'Photohari', price: 700, description: 'Lorem ipsum dolor sit amet.' },
+        { id: '4', title: 'Astrillian', price: 700, description: 'Lorem ipsum dolor sit amet.' }
+    ]
+};
+
+const itemReducer = (state = init, action) => {
+    return state;
+};
+
+export default itemReducer;
