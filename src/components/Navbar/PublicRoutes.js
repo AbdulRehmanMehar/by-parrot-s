e4b589a718a6e7817e9b067e522491fe43ecad6e
Abdul Rehman <mehars.6925@gmail.com>
2019-03-19 14:41:12 +0500

    routes for non-auth

---
+++ src/components/Navbar/PublicRoutes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PublicRoutes = (props) => {
+    return (
+        <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/register">Register</Link>
+            </li>
+        </ul>
+    );
+};
+
+export default PublicRoutes;
