c7069a05f25b42f6a8e5404d8e14c50e6d10bd08
Abdul Rehman <mehars.6925@gmail.com>
2019-03-19 14:39:16 +0500

    handling login with redux

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,16 @@
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import firebase from '../Firebase';
+import { loginUser, resetPassword } from '../store/actions/authActions';
+ 
+class Login extends Component {
 
-export default class Login extends Component {
-
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             email: '',
             password: '',
-            errors: [],
-            redirect: false,
         };
-    }
-
-    componentDidMount() {
-        firebase.auth().onAuthStateChanged(user => {
-            if (user !== null) this.setState({ redirect: true });
-            else this.setState({ redirect: false })
-        });
     }
 
     setEmail(event) {
@@ -30,47 +22,17 @@ export default class Login extends Component {
     }
 
     handlePwdReset(event) {
-        firebase.auth().sendPasswordResetEmail(this.state.email)
-            .then(_ => {
-                let errors = [];
-                let data = 'Password reset instructions were sent on the email you provided.';
-                errors.filter(error => error.type !== 'general' && error.data !== data);
-                errors.push({type: 'general', data: data});
-                this.setState({errors: errors})
-            }).catch(error => console.error(error));
         event.preventDefault();
+        this.props.resetPwd(this.state);
     }
 
     login(event) {
-        firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-            .then(response => this.setState({redirect: true}))
-            .catch(error => {
-                console.log(error);
-                let code = (error.code.split('/').pop() === 'user-not-found') ? 'email' : 'password';
-                let data = error.message;
-                let errors = [];
-                errors.push({type: code, data: data});
-                this.setState({errors: errors});
-            });
         event.preventDefault();
+        this.props.login(this.state);
     }
 
     render() {
-        let general = '',
-            emailErrors = '',
-            passwordErrors = '',
-            passwordResetLink;
-        this.state.errors.forEach(error => {
-            if(error.type === 'general'){
-                general += `<small>${error.data}</small><br />`;
-            }else if(error.type === 'email'){
-                emailErrors += `<small>${error.data}</small><br />`;
-            }else{
-                passwordErrors += `<small>${error.data}</small><br />`;
-                passwordResetLink = <span onClick={this.handlePwdReset.bind(this)} style={{float: "right", color: "#00f", cursor: "pointer"}}>Reset Password?</span>;
-            }
-        });
-        if(this.state.redirect) return <Redirect to="/dashboard" />;
+        if(this.props.auth.uid) return <Redirect to="/dashboard" />;
         return (
             <div className="container mt-4">
                 <div className="card">
@@ -80,15 +42,29 @@ export default class Login extends Component {
                             <div className="form-group">
                                 <label htmlFor="exampleInputEmail1">Email address</label>
                                 <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" onChange={this.setEmail.bind(this)} />
-                                <div className="form-text text-danger" dangerouslySetInnerHTML={{ __html: emailErrors }}></div>
+                                <small className="form-text text-danger">
+                                    { this.props.emailError }
+                                </small>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="exampleInputPassword1">Password</label>
                                 <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" onChange={this.setPassword.bind(this)} />
-                                <div className="d-inline form-text text-danger" dangerouslySetInnerHTML={{ __html: passwordErrors }}></div>
-                                <small>{passwordResetLink}</small>
+                                <small className="form-text text-danger">
+                                    { this.props.passwordError }
+                                    { 
+                                        this.props.passwordError !== null ? 
+                                            <span 
+                                                onClick={this.handlePwdReset.bind(this)} 
+                                                style={{color: "#00f", float: "right", cursor: "pointer"}}>
+                                                Forgot Password?
+                                            </span> 
+                                        :   null 
+                                    }
+                                </small>
                             </div>
-                            <div className="form-text text-success" dangerouslySetInnerHTML={{ __html: general }}></div>                            
+                            <small className="form-text mb-2 text-success">
+                                {this.props.info}
+                            </small>
                             <button type="submit" className="btn btn-primary" disabled={this.state.email === '' || this.state.password === ''}>Login</button>
                         </form>
                     </div>
@@ -97,3 +73,21 @@ export default class Login extends Component {
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        info: state.auth.info,
+        auth: state.firebase.auth,
+        emailError: state.auth.emailError,
+        passwordError: state.auth.passwordError,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        login: (credentails) => dispatch(loginUser(credentails)),
+        resetPwd: (credentails) => dispatch(resetPassword(credentails))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
