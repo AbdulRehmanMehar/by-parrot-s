2e592556ab2bc245519fa39680113f73e1402086
Abdul Rehman <mehars.6925@gmail.com>
2019-03-19 14:38:29 +0500

    connected to redux and loaded dummy data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Home extends Component{
+class Home extends Component{
     render(){
+        let items = this.props.items.map(item => (
+            <div id={`item-${item.id}`}>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+            </div>
+        ));
         return(
             <div className="container">
                 <h1>Hello World!</h1>
+                {items}
             </div>
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        items: state.item.items
+    };
+};
+
+export default connect(mapStateToProps)(Home);
