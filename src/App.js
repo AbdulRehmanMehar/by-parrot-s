cb6a2738eec0ac1233c704b016337455c8fbfba0
Abdul Rehman <mehars.6925@gmail.com>
2019-03-21 01:25:46 +0500

    added admin routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Item from './components/Item';
 import Login from './components/Login';
 import Register from './components/Register';
+import Category from './components/Category';
 import Dashboard from './components/Dashboard';
 import UpdateProfile from './components/UpdateProfile';
 
-const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
-  <Route {...rest} render={(props) => {
-    return isAuthenticated ? <Component {...props} /> : <Redirect to={{
-        pathname: '/login',
-        state: { from: props.location }
-      }} />;
-  }} />
-);
-
 class App extends Component {
-  
-  constructor() {
-    super();
-    this.state = {
-      currentuser: null,
-      isAuthenticated: false,
-    }
-  }
-
   render() {
-    let isAuthenticated = this.props.auth.uid !== null;
     return (
       <Router>
         <div>
@@ -36,8 +19,10 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
-          <PrivateRoute path="/dashboard" component={_ => <Dashboard user={this.state.currentuser} />} isAuthenticated={isAuthenticated} />
-          <PrivateRoute path="/updateprofile" component={_ => <UpdateProfile user={this.state.currentuser} />} isAuthenticated={isAuthenticated} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/update-profile" component={UpdateProfile} />
+          <Route path="/manage-items" component={Item} />
+          <Route path="/manage-categories" component={Category} />
         </div>
       </Router>
     )
