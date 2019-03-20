bc32953113688b373bce1388266c2d84a85f6c00
Abdul Rehman <mehars.6925@gmail.com>
2019-03-21 01:27:36 +0500

    added admin routes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,43 @@
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { firestoreConnect } from 'react-redux-firebase';
 
-export default class Dashboard extends Component{   
+class Dashboard extends Component{   
     render(){
+        if(!this.props.auth.uid) return <Redirect to="/login" />;
         return(
             <div className="container">
                 <div className="jumbotron mt-4">
-                    <h1 className="font-weight-light">Hello, {this.props.user.displayName || 'Newbie'}!</h1>
+                    <h1 className="font-weight-light">Hello, {this.props.auth.displayName}!</h1>
                     <p className="lead">
                         Welcome to <b>By Parrot's</b>, the perfect place for parrot lovers.
                     </p>
                     <hr className="my-4" />
                     <p>Actions: </p>
-                    <Link to="#" className="btn btn-primary">Update Profile</Link>
-                    {/* <a class="btn btn-primary" href="#" role="button">Learn more</a> */}
+                    <Link to="/update-profile" className="btn btn-primary mr-2">Update Profile</Link>
+                    { this.props.profile.admin ? 
+                        <span>
+                            <Link to="manage-items" className="btn btn-primary mr-2">Manage Items</Link>
+                            <Link to="manage-categories" className="btn btn-primary mr-2">Manage Categories</Link>
+                            <Link to="view-orders"  className="btn btn-primary mr-2">View Orders</Link>
+                        </span>
+                    : null }
                 </div>
             </div>
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.firebase.auth,
+        profile: state.firebase.profile
+    };
+};
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect(['users']),
+)(Dashboard);
